b9c6b0a05dbd40390aafb8aaba519fce18003027
Mustafa <mhassanzadeh777@gmail.com>
2023-01-13 21:10:56 -0700

    Added basic mockup for header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
+import Selection from './components/Selections';
+import siteLogo from './assets/images/logo.svg';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <header>
+        <img src={siteLogo} alt="logo" />
+        <section className="score-section">
+          <h1>SCORE</h1>
+          <h2>0</h2>
+        </section>
+      </header>
+      <Selection />
     </div>
   )
 }
