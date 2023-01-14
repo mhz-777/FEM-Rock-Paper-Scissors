b3a230308676d9f5b4cb24a8660603d172614b71
Mustafa <mhassanzadeh777@gmail.com>
2023-01-13 21:09:56 -0700

    Added selections component files

---
+++ src/components/Selections.tsx
@@ -0,0 +1,12 @@
+import './Selections.css';
+
+const Selection = () => {
+    return (
+        <div className="selections-container">
+            
+            
+        </div>
+    );
+}
+
+export default Selection;
