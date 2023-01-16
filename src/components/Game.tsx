3ed2a7bcc436087da634ba1493c1f90ecd393de4
Mustafa <mhassanzadeh777@gmail.com>
2023-01-16 15:46:19 -0700

    renamed component

---
+++ src/components/Game.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './Game.css';
+
+const Game = () => {
+
+    const [userSelect, applySelect] = useState('');
+    const [isReady, setReady] = useState(false);
+
+    const handleUserClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        applySelect(event.currentTarget.id);
+        setReady(true);
+    }
+
+    const cpuRandomizer = () => {
+        let cpuChoice = Math.floor(Math.random() * 3);
+        console.log(cpuChoice);
+        
+    }
+
+    return (
+        <section className="game-container">
+            <section className="game-selections">
+                <div className="btn-wrapper" id='wrapper-rock'>
+                    <button className="rock-btn" id='rock' onClick={handleUserClick}>
+                        ROCK
+                    </button>
+                </div>
+                <div className="btn-wrapper" id='wrapper-paper'>
+                    <button className="paper-btn" id="paper" onClick={handleUserClick}>
+                        PAPER
+                    </button>
+                </div>
+                <div className="btn-wrapper" id='wrapper-scissors'>
+                    <button className="scissors-btn" id="scissors" onClick={handleUserClick}>
+                        SCISSORS
+                    </button>
+                </div>
+            </section>
+        </section>
+    );
+}
+
+export default Game;
