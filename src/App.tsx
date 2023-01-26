79bf84c4a5723bc0866e3b764734668c010c4e0c
Mustafa <mhassanzadeh777@gmail.com>
2023-01-26 15:21:36 -0700

    Added state to handle revealing rules, added button to reveal rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import './App.css'
 import Game from './components/Game';
+import Rules from './components/Rules';
 import siteLogo from './assets/images/logo.svg';
+import { useEffect, useState } from 'react';
 
-function App() {
+
+const App: React.FC = () => {
+
+
+  //state for game score
+  const [gameScore, setGameScore] = useState<number>(0);
+
+  //update with new val
+  const incrementScore = () =>{
+    setGameScore(gameScore + 1);
+  }
+
+  //state to reveal rules
+  const [isClicked, setClicked] = useState<boolean>(false);
+
+  //handle click for rules btn
+  const handleClick = () => {
+    setClicked(!isClicked);
+  }
 
   return (
     <div className="App">
@@ -11,13 +31,20 @@ function App() {
           <img src={siteLogo} alt="logo" />
           <section className="score-section">
             <h3>SCORE</h3>
-            <h1>0</h1>
+            <h1>{gameScore}</h1>
           </section>
         </div>
       </header>
       <main>
         <Game />
+        <Rules
+          isClicked={isClicked}
+          handleClick = {handleClick}
+        />
       </main>
+      <footer>
+        <button className={`${isClicked === false ? 'rules-btn' : 'rules-btn-hidden'}`}onClick={handleClick}>RULES</button>
+      </footer>
     </div>
   )
 }
