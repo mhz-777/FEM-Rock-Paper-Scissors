e5ebf16b3f7a8955349dc266d31d081d7d54ea4d
Mustafa <mhassanzadeh777@gmail.com>
2023-01-26 15:20:14 -0700

    Passed down state props from parent, added JSX for rules section

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -1,8 +1,25 @@
+import React, { useState } from 'react';
 import './Rules.css';
+import rulesSVG from '../assets/images/image-rules.svg';
+import closeSVG from '../assets/images/icon-close.svg';
 
+interface Props {
+    isClicked: boolean;
+    handleClick: ()=>void;
+}
 
-const Rules = () => {
+const Rules: React.FC<Props> = ({isClicked, handleClick}) => {
 
+    return (
+        <section className={`${isClicked === true ? 'rules-section' : 'rules-section-hidden'}`}>
+            <section className="rules">
+                <h1 className="rules-header">RULES</h1>
+                <img src={rulesSVG} alt="rules" />
+                <button className="close-rules-btn" onClick={handleClick}></button>
+            </section>
+        </section>
+        
+    );
 }
 
 export default Rules;
